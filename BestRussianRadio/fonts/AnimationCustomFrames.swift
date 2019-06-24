4b1991a2dd54e78bf2108931cbcf8fed2c85ae01
Maxim Spiridonov <taijased@gmail.com>
2019-06-24 19:20:12 +0300

    version 1

---
+++ BestRussianRadio/fonts/AnimationCustomFrames.swift
@@ -0,0 +1,24 @@
+
+import UIKit
+
+class AnimationCustomFrames {
+    
+    class func createFrames() -> [UIImage] {
+    
+        // Setup "Now Playing" Animation Bars
+        var animationFrames = [UIImage]()
+        for i in 0...3 {
+            if let image = UIImage(named: "equalizer-\(i)") {
+                animationFrames.append(image)
+            }
+        }
+        
+        for i in stride(from: 2, to: 0, by: -1) {
+            if let image = UIImage(named: "equalizer-\(i)") {
+                animationFrames.append(image)
+            }
+        }
+        return animationFrames
+    }
+
+}
